52794e854bd5740c18c61e8046bf5513732fadf2
Jack Hsieh <jack@jackhsieh.com>
2015-05-02 23:40:45 -0700

    Selection list for transportation

--- www/app/controller/controllers.js
+++ www/app/controller/controllers.js
@@ -226,6 +226,20 @@ app.controller('evacueeCtrl', function($scope, $rootScope, drtrackService) {
 });
 
 app.controller('checkinCtrl', function($scope, $ionicPopup) {
+  $scope.transports = [
+    {id: 1, name: "The Bus", type: "Bus", capacity: 30, space: 1},
+    {id: 2, name: "The Train", type: "Train", capacity: 100, space: 50},
+    {id: 3, name: "The Airplane", type: "Airplane", capacity: 200, space: 20},
+    {id: 4, name: "The Ship", type: "Ship", capacity: 1000, space: 0},
+    {id: 5, name: "The Bus 2", type: "Bus", capacity: 30, space: 1},
+    {id: 6, name: "The Train 2", type: "Train", capacity: 100, space: 50},
+    {id: 7, name: "The Airplane 2", type: "Airplane", capacity: 200, space: 20},
+    {id: 8, name: "The Ship 2", type: "Ship", capacity: 1000, space: 0},
+    {id: 9, name: "The Bus 3", type: "Bus", capacity: 30, space: 1},
+    {id: 10, name: "The Train 3", type: "Train", capacity: 100, space: 50},
+    {id: 11, name: "The Airplane 3", type: "Airplane", capacity: 200, space: 20},
+    {id: 12, name: "The Ship 3", type: "Ship", capacity: 1000, space: 0}
+  ];
   $scope.scanner = {};
   $scope.scanDatas = [];
   $scope.manualCheckin = function() {
